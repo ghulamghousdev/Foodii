ac063a27af70af98ab0b4a75c8a5edad433c739a
ghulamghousdev <itsghulamghous@gmail.com>
2020-05-03 04:22:09 +0500

    📦 NEW: Getting data using API

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
- 
+import axios from 'axios';
+
+async function get(query){
+   const result = await axios.get(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+   console.log(result);
+}
+get();
