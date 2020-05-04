4d8fa715536dacc8cd64ee60f4eb353ad88fbfd7
ghulamghousdev <itsghulamghous@gmail.com>
2020-05-04 21:55:27 +0500

    📦 NEW: Add spinner

---
+++ src/js/views/base.js
@@ -0,0 +1,31 @@
+export const elements = {
+    searchInput: document.querySelector('.search__field'),
+    searchForm: document.querySelector('.search'),
+    searchResultList: document.querySelector('.results__list'),
+    searchResult: document.querySelector('.results'),
+    searchResPages: document.querySelector('.results__pages')   
+};
+
+export const elementStrings = {
+    loader: 'loader'
+}
+
+//Rendering spinner
+export const  renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+}
+
+//clear spinner
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if(loader){
+        loader.parentElement.removeChild(loader);
+    }
+}
