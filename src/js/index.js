e8215940fef55c50b5e17f43b1e15b2115347a98
ghulamghousdev <itsghulamghous@gmail.com>
2020-05-04 21:53:19 +0500

    📦 NEW: Add event listeners for pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,53 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader} from './views/base';
 
-async function get(query){
-   const result = await axios.get(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-   console.log(result);
+/*Global State of the app which contains 
+* Search Object
+* Current Recipe Object
+* Shopping List Object
+* Liked Recipes
+*/
+
+const state = {};
+let res;
+
+//Function to control search
+const controlSearch = async () => {
+
+    //Get query from view
+    const query = searchView.getInput(); 
+
+    if (query){
+        
+        //New Search object and add it to state
+        state.search = new Search(query);
+
+        //prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchResult);
+
+        //Search for recipes
+        res = await state.search.getResults();
+
+        //render results on UI
+        clearLoader();
+        searchView.renderResults(res);
+    }
 }
-get();
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const button = e.target.closest('.btn-inline');
+    if(button){
+        const gotToPage = parseInt(button.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(res, gotToPage);
+    } 
+
+});
