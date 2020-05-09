322fa6fa8570c060072eb2f0b14f3221cd0c25c7
ghulamghousdev <itsghulamghous@gmail.com>
2020-05-09 21:33:53 +0500

    📦 NEW: Implemented Local storage for Liked recipies

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -11,6 +11,10 @@ export default class Likes {
             img
         }
         this.likes.push(like);
+
+        //Persist data in local storage
+        this.persistData();
+
         return like;
     }
 
@@ -20,6 +24,9 @@ export default class Likes {
         })
 
         this.likes.splice(index, 1);
+
+        //Persist data in local storage
+        this.persistData();
     }
 
     isLiked(id) {
@@ -28,5 +35,16 @@ export default class Likes {
 
     getNumLikes() {
         return this.likes.length;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'));
+        
+        // Restoring likes from the localStorage
+        if (storage) this.likes = storage;
     }
 }
